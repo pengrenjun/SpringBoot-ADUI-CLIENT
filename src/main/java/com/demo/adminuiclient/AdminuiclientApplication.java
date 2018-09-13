d897f5be30ed37454ea4e50eed065cc575e18c4f
pengrj <pengrj@emrubik.com>
2018-09-13 17:13:02 +0800

    Initial commit

---
+++ src/main/java/com/demo/adminuiclient/AdminuiclientApplication.java
@@ -0,0 +1,12 @@
+package com.demo.adminuiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminuiclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminuiclientApplication.class, args);
+    }
+}
